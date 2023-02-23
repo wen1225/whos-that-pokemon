a8fa6affc03cd215a3bf6beeaa310d6db213a186
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2023-02-22 19:22:23 -0800

    frontend connected to backend

---
+++ dev/PokemonClient/game-v1/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios from "axios";
+
+import api from './api/axiosConfig';
+import React, {useState, useEffect} from 'react';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  
+  
+  const getPokemon = () =>{
+    api
+      .get("/api/v1/pokemon/random")
+      .then((response) => {
+        console.log(response);
+        setPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+
+
+  };
+
+  useEffect(() => {
+    getPokemon();
+  },[])
+
+  
+
+  return (
+    <div>
+      <h1>Who's That Pokemon</h1>
+      Test <button onClick={getPokemon}>get random pokemon</button>
+        
+        <h1>{pokemon.name} </h1>
+        <h1>{pokemon.entry}</h1>
+        <h1>{pokemon.types}</h1>
+        <h1>{pokemon.region}</h1>
+        
+      
+    </div>
+  );
+};
+
+export default App;
