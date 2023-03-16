94181d0b0a4f73a68228cc2cfc1794f79f6d5144
Ho <kenho12251999@gmail.com>
2023-03-16 13:49:33 -0700

    partial completion of frontend for the single player page

--- dev/PokemonClient/game-v1/src/pages/SinglePlayer/index.jsx
+++ dev/PokemonClient/game-v1/src/pages/SinglePlayer/index.jsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import styles from "./styles.module.css";
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import {Typography } from '@mui/material'
+
 import Timer from '../../components/timer';
+
 
 function SinglePlayer() {
     return (
     <div>
-        <h1>Who's That Pokemon Single Player Mode</h1>
-        <Timer seconds={10}/>
+        <React.Fragment>
+            <Box position='absolute' top='8%' bottom='10%' left='10%' sx={{m: 0, p: 0, bgcolor: '#DCDBE7', height: '72%', width: '80%', boxShadow: 10}} />
+            <Typography variant='h1' position='absolute' top='8%' bottom='10%' left='10%' sx={{m: 0, p: 0, bgcolor: '#DCDBE7', height: '72%', width: '80%'}}>00:04:31</Typography>
+            <Timer />
+        </React.Fragment>
     </div>
     );
 }
