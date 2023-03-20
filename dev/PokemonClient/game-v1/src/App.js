382801dc6790237f396b3987444ac3e6dbe1f1d0
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2023-03-19 19:25:12 -0700

    further work on cards

--- dev/PokemonClient/game-v1/src/App.js
+++ dev/PokemonClient/game-v1/src/App.js
@@ -11,7 +11,7 @@ function App() {
    
  return(
    <div>
-    <SinglePlayer />
+    {/* <SinglePlayer /> */}
     <GetRandomPokemon />
     {/* <GetRandomPokemon /> */}
     {/* <GetOtherPokemon />
--- dev/PokemonClient/game-v1/src/components/getRandomPokemon.js
+++ dev/PokemonClient/game-v1/src/components/getRandomPokemon.js
@@ -1,74 +1,143 @@
 //import './App.css';
 import api from '../api/axiosConfig';
-import React, {useState, useEffect, Component} from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import useSound from 'use-sound';
 import { Howl } from 'howler';
 import './getRandomPokemon.css';
 
 //Gets the random "correct" Pokemon for the round. Plays sound
 
-function GetRandomPokemon(){
-    const [pokemon, setPokemon] = useState([]);
-    const [src, setsrc] = useState([]);
-    
-    const play = (src) =>{
-      const sound = new Howl({
-        src,
-        html5:true,
-      });
-      sound.play()
+function GetRandomPokemon() {
+  const [pokemon, setPokemon] = useState([]);
+  const [src, setsrc] = useState([]);
+
+  //START: Handles ability to click a hint button once
+  const [showIdContent, setShowIdContent] = useState(false);
+  const [showRegionContent, setShowRegionContent] = useState(false);
+  const [showTypeContent, setShowTypeContent] = useState(false);
+  const [showEntryContent, setShowEntryContent] = useState(false);
+
+  const [isIdClickable, setIsIdClickable] = useState(true);
+  const [isRegionClickable, setIsRegionClickable] = useState(true);
+  const [isTypeClickable, setIsTypeClickable] = useState(true);
+  const [isEntryClickable, setIsEntryClickable] = useState(true);
+
+
+  const handleIdClick = () => {
+    if (isIdClickable) {
+      setShowIdContent(true);
+      setIsIdClickable(false);
     }
+  };
 
-    const getPokemonComponent = () =>{
-        api
-          .get("/api/v1/pokemon/random")
-          .then((response) => {
-            console.log(response);
-            setPokemon(response.data);
-            setsrc(response.data.cry)
-            play(response.data.cry);
-            
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+  const handleRegionClick = () => {
+    if (isRegionClickable) {
+      setShowRegionContent(true);
+      setIsRegionClickable(false);
+    }
+  };
+  const handleTypeClick = () => {
+    if (isTypeClickable) {
+      setShowTypeContent(true);
+      setIsTypeClickable(false);
+    }
+  };
+  const handleEntryClick = () => {
+    if (isEntryClickable) {
+      setShowEntryContent(true);
+      setIsEntryClickable(false);
+    }
+  };
+  //END:
 
-      
-      
-        
-      return(
-        <div> 
-        
-         
-            <div className='box'>
-              <button  onClick={() => {getPokemonComponent();}}>get random pokemon</button>
-              <img  className='pokemon-bg' src = {pokemon.sprite} />
-            </div>
-           
-        
-          <section>
-            <div className = "cards">
-              <div className = "card">
-                <h1>{pokemon.name}</h1>
-              </div>
-              <div className = "card">
-                <h1>{pokemon.types}</h1>
-              </div>
-                <div className = "card" >
-                <h1>{pokemon.region}</h1>
-              </div>
-              <div className = "card">
-                <h1>{pokemon.entry}</h1>
-              </div>
-            </div>
-          </section>
-         
-        
+
+  //Plays Pokemon's cry
+  const play = (src) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play()
+  }
+
+  //Get's Pokemon Data from API
+  const getPokemonComponent = () => {
+    api
+      .get("/api/v1/pokemon/random")
+      .then((response) => {
+        console.log(response);
+        setPokemon(response.data);
+        setsrc(response.data.cry)
+        play(response.data.cry);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
+
+
+  return (
+    <div>
+
+      {/* Box where the Pokemon Appear */}
+      <div className='box'>
+        <button onClick={() => { getPokemonComponent(); }}>get random pokemon</button>
+        <img className='pokemon-bg' src={pokemon.sprite} />
+      </div>
+
+
+      {/* Container for all the hint cards */}
+      <div className="card-grid">
+
+        {/* Card for Pokedex Number */}
+        <div className="card" onClick={() => handleIdClick()}>
+          {showIdContent ? (
+            <h1 className='font'>{pokemon.id}</h1>
+          ) : (
+            <h1 className = 'font'> Pokedex Id: ?</h1>
+          )}
         </div>
-      
-        
-      );
+
+
+        {/* Card for Pokemon type */}
+        <div className="card" onClick={() => handleTypeClick()}>
+          {showTypeContent ? (
+            <h1 className='font'>Pokemon Type(s): {pokemon.types}</h1>
+          ) : (
+            <h1 className = 'font'> Pokemon Type: ?</h1>
+          )}
+        </div>
+
+
+        {/* Card for Pokemon Region */}
+        <div className="card" onClick={() => handleRegionClick()}>
+          {showRegionContent ? (
+            <h1 className='font'>Region: {pokemon.region}</h1>
+          ) : (
+            <h1 className = 'font'> Pokemon Region: ?</h1>
+          )}
+        </div>
+
+        {/*Card for Pokemon Pokedex entry */}
+        <div className="card" onClick={() => handleEntryClick()}>
+          {showEntryContent ? (
+            <h1 className='font'> {pokemon.entry}</h1>
+          ) : (
+            <h1 className = 'font'> Pokedex Entry: ?</h1>
+          )}
+        </div>
+      </div>
+
+
+
+    </div>
+
+
+  );
 
 };
 export default GetRandomPokemon;
