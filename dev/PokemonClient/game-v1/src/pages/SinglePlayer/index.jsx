cad673f6940e36376dd0352ed535ad794818cc34
Ho <kenho12251999@gmail.com>
2023-04-11 13:47:27 -0700

    completed single player responsive page

--- dev/PokemonClient/game-v1/src/pages/SinglePlayer/index.jsx
+++ dev/PokemonClient/game-v1/src/pages/SinglePlayer/index.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, Component } from 'react';
 import styles from "./styles.module.css";
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import {Button, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 
 
 
@@ -15,21 +15,11 @@ function SinglePlayer() {
     // const [roundNumber, setRoundNumber] = useState(0);
 
     return (
-    <div>
-         
-        <React.Fragment>
-            <Box position='absolute' top='5%' bottom='10%' left='5%' justifyContent="center"  alignItems="center" sx={{m: 0, p: 0, bgcolor: '#DCDBE7', height: '90%', width: '90%', boxShadow: 10}} >
-                <GetRandomPokemon />
-    
-            </Box>
-
-            
-            
-       
-        </React.Fragment>
-       
         
-    </div>
+        <React.Fragment>
+            <Box position='absolute' top='5%' bottom='10%' left='5%' justifyContent="center" alignItems="center" sx={{ m: 0, p: 0, bgcolor: '#DCDBE7', height: '90%', width: '90%', boxShadow: 10 }} />
+            <GetRandomPokemon />
+        </React.Fragment>
     );
 }
 
