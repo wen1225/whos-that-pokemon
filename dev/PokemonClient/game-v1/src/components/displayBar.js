d3830ff02fb4fa5a931e53d83677cd75cd0500bd
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2023-04-18 13:55:25 -0700

    Player can now see if their choice was correct or incorrect

--- dev/PokemonClient/game-v1/src/components/displayBar.js
+++ dev/PokemonClient/game-v1/src/components/displayBar.js
@@ -3,26 +3,50 @@ import React, { useState, useEffect, Component } from 'react';
 // import  './getRandomPokemon.css';
 import './displayBar.css';
 import './getRandomPokemon'
+import { blue, green } from '@mui/material/colors';
 
-export function DisplayBar(props) {
+ export function DisplayBar(props) {
   const { pokemonData, correctPokemon } = props;
+  const [bgColor, setBgColor] = useState('#EFEFEF');
 
 
+  /*Handles Clicks on Pokemon image.
+  Clicks on the correct Pokemon will make its background green.
+  Clicks on the incorrect Pokemon will make its background red.
+  */
+  const handleClick = (pokemon) =>{
+    if(pokemon.name == correctPokemon.name){
+      console.log("w");
+      setBgColor((prevBgColor) => ({
+        ...prevBgColor,
+        [pokemon.id]: '#AFE1AF', //green
+      }));
+    }
+    else{
+      console.log("l");
+      setBgColor((prevBgColor) => ({
+        ...prevBgColor,
+        [pokemon.id]: '#FA8072 ', //red
+      }));
+    }
+    setTimeout(() => {
+      setBgColor((prevBgColor) => ({
+        ...prevBgColor,
+        [pokemon.id]: '#EFEFEF',
+      }));
+    }, 3000);
+  }
 
+  
+    
+  //Returns the box where all the clickable Pokemon images are displayed
   return (
-    <div className='box'>
+    <div className='box' >
       {pokemonData.map((pokemon, index) => (
         <div key={index}>
-          <img className='pokemon-bg' src={pokemon.sprite} alt={pokemon.name} onClick={() => {
-            if (pokemon.name == correctPokemon.name) {
-              console.log("W");
-            }
-            else {
-              console.log("L");
-              //and update point counter 
-              //handlePoints(500);
-            }
-          }} />
+          <img className='pokemon-bg' src={pokemon.sprite} alt={pokemon.name}  style={{ backgroundColor: bgColor[pokemon.id] }} onClick={() => handleClick(pokemon)}>
+
+          </img>
         </div>
       ))}
     </div>
