4914707a5b9bb798af9d1c3b643ed835acd96cfc
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2023-04-18 14:16:06 -0700

    Only one POkemon can be selected now within a time limit of 2 seconds

--- dev/PokemonClient/game-v1/src/components/displayBar.js
+++ dev/PokemonClient/game-v1/src/components/displayBar.js
@@ -1,57 +1,62 @@
 import api from '../api/axiosConfig';
-import React, { useState, useEffect, Component } from 'react';
-// import  './getRandomPokemon.css';
+import React, { useState } from 'react';
 import './displayBar.css';
-import './getRandomPokemon'
 import { blue, green } from '@mui/material/colors';
 
- export function DisplayBar(props) {
+export function DisplayBar(props) {
   const { pokemonData, correctPokemon } = props;
-  const [bgColor, setBgColor] = useState('#EFEFEF');
+  const [bgColor, setBgColor] = useState({});
+  const [selectionMade, setSelectionMade] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
 
-  /*Handles Clicks on Pokemon image.
+   /*Handles Clicks on Pokemon image.
   Clicks on the correct Pokemon will make its background green.
   Clicks on the incorrect Pokemon will make its background red.
   */
-  const handleClick = (pokemon) =>{
-    if(pokemon.name == correctPokemon.name){
-      console.log("w");
-      setBgColor((prevBgColor) => ({
-        ...prevBgColor,
-        [pokemon.id]: '#AFE1AF', //green
-      }));
+  const handleClick = (pokemon) => {
+    if (!selectionMade) {
+      if (pokemon.name === correctPokemon.name) {
+        console.log('Correct Pokemon clicked!');
+        setBgColor((prevBgColor) => ({
+          ...prevBgColor,
+          [pokemon.id]: '#AFE1AF', //green
+        }));
+        setIsCorrect(true);
+      } else {
+        console.log('Incorrect Pokemon clicked.');
+        setBgColor((prevBgColor) => ({
+          ...prevBgColor,
+          [pokemon.id]: '#FA8072', //red
+        }));
+        setIsCorrect(false);
+      }
+      //Resets the bg of the Pokemon after 2000 milliseconds
+      setTimeout(() => {
+        setBgColor((prevBgColor) => ({
+          ...prevBgColor,
+          [pokemon.id]: '',
+        }));
+        setSelectionMade(false);
+      }, 2000);
+      setSelectionMade(true);
     }
-    else{
-      console.log("l");
-      setBgColor((prevBgColor) => ({
-        ...prevBgColor,
-        [pokemon.id]: '#FA8072 ', //red
-      }));
-    }
-    setTimeout(() => {
-      setBgColor((prevBgColor) => ({
-        ...prevBgColor,
-        [pokemon.id]: '#EFEFEF',
-      }));
-    }, 3000);
-  }
+  };
 
-  
-    
   //Returns the box where all the clickable Pokemon images are displayed
   return (
-    <div className='box' >
+    <div className='box'>
       {pokemonData.map((pokemon, index) => (
         <div key={index}>
-          <img className='pokemon-bg' src={pokemon.sprite} alt={pokemon.name}  style={{ backgroundColor: bgColor[pokemon.id] }} onClick={() => handleClick(pokemon)}>
-
-          </img>
+          <img className='pokemon-bg' src={pokemon.sprite} alt={pokemon.name} style={{ backgroundColor: bgColor[pokemon.id] }} onClick={() => handleClick(pokemon)}
+          />
+          
         </div>
       ))}
+      
     </div>
+    
   );
 }
+
 export default DisplayBar;
-
-
