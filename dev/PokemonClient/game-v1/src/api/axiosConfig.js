58613f4ecbb2d73e419d0e208867bce2ad7b2677
Abhinav Selvaraj <abhipuli2579@gmail.com>
2023-05-07 21:01:22 -0700

    updated Ipv4 address

--- dev/PokemonClient/game-v1/src/api/axiosConfig.js
+++ dev/PokemonClient/game-v1/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'http://ec2-3-129-14-21.us-east-2.compute.amazonaws.com:8080/',
+    baseURL: 'http://ec2-3-16-151-99.us-east-2.compute.amazonaws.com:8080/',
     //headers: {"ngrok-skip-browser-warning": "true"}
 });
